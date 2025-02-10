1a4d85106d53fe79ace3862197ae6f1fc87054a0
Ethan Mina <mina-ethanchristian@c-fo.com>
2025-02-10 14:13:28 +0800

    [AOC 2] Added a navbar and models for the onboarding checklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_08_165056) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_09_153541) do
+  create_table "main_tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "duration"
+    t.integer "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_main_tasks_on_users_id"
+  end
+
+  create_table "sub_tasks", force: :cascade do |t|
+    t.integer "authority"
+    t.string "name"
+    t.string "description"
+    t.datetime "duration"
+    t.integer "main_tasks_id", null: false
+    t.integer "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["main_tasks_id"], name: "index_sub_tasks_on_main_tasks_id"
+    t.index ["users_id"], name: "index_sub_tasks_on_users_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +48,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_08_165056) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "main_tasks", "users", column: "users_id"
+  add_foreign_key "sub_tasks", "main_tasks", column: "main_tasks_id"
+  add_foreign_key "sub_tasks", "users", column: "users_id"
 end
